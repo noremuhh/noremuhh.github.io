e698803b79a72bb912dc639ad25bb524eed8adf0
Norma <kamacho_24@eagles.ewu.edu>
2019-04-05 02:30:14 -0700

    Update app.js

--- javascripts/app.js
+++ javascripts/app.js
@@ -87,40 +87,6 @@ jQuery(document).foundation();
       }
     });
 
-  <?php
-
-    $to = "normasweblink@outlook.com";
-  
-    $name = $_POST['name'];
-    $subject = $_POST['subject'];
-  
-    $body = stripslashes($_POST['message'];
-  
-    $from = $_POST['email'];
-    $headers = "Reply-To: " . $from;
-    $error_messages = array();
-  
-    if( strlen($name) < 1 ) {
-      $error_messages[] = "name required";
-    }
-  
-    if( strlen($body) < 1 ) {
-      $error_messages[] = "comment required";
-    }
-  
-    if( strlen($from) < 1 ) {
-      $error_messages[] = "email required";
-    }
-  
-    if(sizeof($error_messages) == 0 && mail($to, $subject, $body, $headers)) {
-      echo("success");
-    } else {
-      foreach( $error_messages as $error_message ) {
-        echo "$error_message <br />";
-      }
-    }
-?>
-
     if($('.masonry-container').length > 0) {
 
       $('.masonry-container').each(function() {
